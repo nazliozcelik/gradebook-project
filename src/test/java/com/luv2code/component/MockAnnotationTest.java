02b71b4ab41a4fef9b6ad349c5f7a936a0143044
nazliozcelik <nazliozelikk@icloud.com>
2022-09-26 16:47:53 +0300

    mocking with MockBean

---
+++ src/test/java/com/luv2code/component/MockAnnotationTest.java
@@ -0,0 +1,72 @@
+package com.luv2code.component;
+
+import com.luv2code.component.dao.ApplicationDao;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import com.luv2code.component.service.ApplicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class MockAnnotationTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    CollegeStudent studentOne;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    //@Mock
+    @MockBean
+    private ApplicationDao applicationDao;
+
+    //@InjectMocks
+    @Autowired
+    private ApplicationService applicationService;
+
+    @BeforeEach
+    void beforeEach(){
+        studentOne.setFirstname("Eric");
+        studentOne.setLastname("Roby");
+        studentOne.setEmailAddress("eric.roby@test.com");
+        studentOne.setStudentGrades(studentGrades);
+    }
+
+    @Test
+    @DisplayName("When & Verify")
+    void assertEqualsTestAddGrades(){
+        when(applicationDao.addGradeResultsForSingleClass(studentGrades.getMathGradeResults())).thenReturn(100.00);
+
+        assertEquals(100, applicationService.addGradeResultsForSingleClass(studentOne.getStudentGrades().getMathGradeResults()));
+
+        verify(applicationDao, times(1)).addGradeResultsForSingleClass(studentGrades.getMathGradeResults());
+    }
+
+    @Test
+    @DisplayName("Find GPA")
+    void assertEqualsTestFindGpa(){
+        when(applicationDao.findGradePointAverage(studentGrades.getMathGradeResults())).thenReturn(85.00);
+
+        assertEquals(85, applicationService.findGradePointAverage(studentOne.getStudentGrades().getMathGradeResults()));
+    }
+
+    @Test
+    @DisplayName("Not Null")
+    void testAssertNotNull(){
+        when(applicationDao.checkNull(studentGrades.getMathGradeResults())).thenReturn(true);
+        assertNotNull(applicationService.checkNull(studentOne.getStudentGrades().getMathGradeResults()));
+    }
+}
