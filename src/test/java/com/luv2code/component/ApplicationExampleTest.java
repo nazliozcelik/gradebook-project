6a54d07417d1293fba824820288798b96ca550ee
nazliozcelik <nazliozelikk@icloud.com>
2022-09-26 11:44:10 +0300

    assertEquals/NotEquals, assertTrue/False and assertNotNull controls added

--- src/test/java/com/luv2code/component/ApplicationExampleTest.java
+++ src/test/java/com/luv2code/component/ApplicationExampleTest.java
@@ -1,13 +1,70 @@
 package com.luv2code.component;
 
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ApplicationExampleTest {
 
-    @Test
-    void bacicTest(){
+    private static int count = 0;
 
+    @Value("${info.app.name}")
+    private String appInfo;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @Value("${info.school.name}")
+    private String schoolName;
+
+    @Autowired
+    CollegeStudent student;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    @BeforeEach
+    public void beforeEach(){
+        count = count + 1;
+        System.out.println("Testing: " + appInfo + " which is " + appDescription + " Version: " + appVersion + ". Execution of test method " + count);
+        student.setFirstname("Eric");
+        student.setLastname("Roby");
+        student.setEmailAddress("eric.roby@test.com");
+        studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList(100.0, 85.0, 76.50, 91.75)));
+        student.setStudentGrades(studentGrades);
+    }
+
+    @Test
+    @DisplayName("Add Grade Results for Student Grades")
+    void addGradeResultsForStudentGrades(){
+        assertEquals(353.25, studentGrades.addGradeResultsForSingleClass(student.getStudentGrades().getMathGradeResults()));
+        assertNotEquals(5, studentGrades.addGradeResultsForSingleClass(student.getStudentGrades().getMathGradeResults()));
+    }
+
+    @Test
+    @DisplayName("Is Grade Greater or Not")
+    void isGradeGreaterOrNot(){
+        assertTrue(studentGrades.isGradeGreater(90, 75), "Should return true");
+        assertFalse(studentGrades.isGradeGreater(44,88), "Should return false");
+    }
+
+    @Test
+    @DisplayName("Check Null for Student Grades")
+    void checkNullForStudentGrades(){
+        assertNotNull(studentGrades.checkNull(student.getStudentGrades().getMathGradeResults()), "Object should not be null");
     }
 }
