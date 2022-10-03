d21eab8af26f61d65980dec2a204f7ee8bdfd026
nazliozcelik <nazliozelikk@icloud.com>
2022-10-03 17:57:44 +0300

    delete grade tdd

--- src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -73,4 +73,17 @@ public class GradebookController {
         return "studentInformation";
     }
 
+    @GetMapping(value = "/grades/{id}/{gradeType}")
+    public String deleteGrade(@PathVariable int id, @PathVariable String gradeType, Model m){
+        int studentId = studentService.deleteGrade(id, gradeType);
+
+        if (studentId == 0) {
+            return "error";
+        }
+
+        studentService.configureStudentInformationModel(studentId, m);
+
+        return "studentInformation";
+    }
+
 }
--- src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -2,6 +2,10 @@ package com.luv2code.springmvc;
 
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.models.GradebookCollegeStudent;
+import com.luv2code.springmvc.models.MathGrade;
+import com.luv2code.springmvc.repository.HistoryGradesDao;
+import com.luv2code.springmvc.repository.MathGradesDao;
+import com.luv2code.springmvc.repository.ScienceGradesDao;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.AfterEach;
@@ -26,6 +30,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -50,6 +55,15 @@ public class GradebookControllerTest {
 
     @Autowired
     private StudentAndGradeService studentService;
+
+    @Autowired
+    private MathGradesDao mathGradeDao;
+
+    @Autowired
+    private ScienceGradesDao scienceGradeDao;
+
+    @Autowired
+    private HistoryGradesDao historyGradeDao;
 
     @Mock
     private StudentAndGradeService studentCreateServiceMock;
@@ -223,6 +237,45 @@ public class GradebookControllerTest {
                 .param("grade", "105.00")
                 .param("gradeType", "literature")
                 .param("studentId", "1")).andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "error");
+    }
+
+    @Test
+    public void deleteAValidGradeHttpRequest() throws Exception{
+        Optional<MathGrade> mathGrade = mathGradeDao.findById(1);
+
+        assertTrue(mathGrade.isPresent());
+
+        MvcResult mvcResult = mockMvc.perform(get("/grades/{id}/{gradeType}", 1, "math")).andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "studentInformation");
+
+        mathGrade = mathGradeDao.findById(1);
+
+        assertFalse(mathGrade.isPresent());
+    }
+
+    @Test
+    public void deleteAValidGradeHttpRequestStudentIdDoesNotExistEmptyResponse() throws Exception{
+        Optional<MathGrade> mathGrade = mathGradeDao.findById(0);
+
+        assertFalse(mathGrade.isPresent());
+
+        MvcResult mvcResult = mockMvc.perform(get("/grades/{id}/{gradeType}", 0, "math")).andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "error");
+    }
+
+    @Test
+    public void deleteNonValidGradeHttpRequest() throws Exception{
+        MvcResult mvcResult = mockMvc.perform(get("/grades/{id}/{gradeType}", 1, "literature")).andExpect(status().isOk()).andReturn();
 
         ModelAndView mav = mvcResult.getModelAndView();
 
